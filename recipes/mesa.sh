1d207ea5be71388b311e1f3161f94a907a2556f2
Vincent Davis Jr <vince@underview.tech>
2022-05-16 00:39:44 -0500

    add mesa recipe to build chain
    For libgbm implementation
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

---
+++ recipes/mesa.sh
@@ -0,0 +1,64 @@
+# Build mesa 22.0.1
+
+
+do_return_version_mesa() {
+  echo "mesa v22.0.1"
+}
+
+
+do_return_depends_mesa() {
+  echo "zlib elfutils libffi llvm wayland wayland-protocols x11 xcb xfixes xshmfence xxf86vm"
+}
+
+
+do_clean_mesa() {
+  rm -rf "${PACKAGES_DIR}/mesa/build"
+}
+
+
+do_fetch_mesa() {
+  msg="Cloning mesa"
+  clone_and_checkout "${PACKAGES_DIR}/mesa" "22.0" "https://gitlab.freedesktop.org/mesa/mesa" "4a8d3189fdb81688c43c9b438c892a3423367c73" "${msg}" || return $FAILURE
+  [[ $? -ne 0 ]] && return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_patch_mesa() {
+  :
+}
+
+
+do_configure_mesa() {
+  meson setup \
+        --prefix="$INSTALLPREFIX" \
+        --libdir="${INSTALLPREFIX}/lib" \
+        "${PACKAGES_DIR}/mesa/build" \
+        "${PACKAGES_DIR}/mesa" || return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_compile_mesa() {
+  ninja -j $BUILDTHREADS -C "${PACKAGES_DIR}/mesa/build" || return $FAILURE
+  return $SUCCESS
+}
+
+
+do_install_mesa() {
+  ninja install -C "${PACKAGES_DIR}/mesa/build" || return $FAILURE
+  return $SUCCESS
+}
+
+
+do_update_artifacts_mesa() {
+  :
+}
+
+
+do_check_is_built_mesa() {
+  [[ -f "${INSTALLPREFIX}/lib/pkgconfig/gl.pc" ]] && return $SUCCESS
+  return $FAILURE
+}
