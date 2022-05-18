31a1cc1cd02455be14a17490b43083343432dfd9
Vincent Davis Jr <vince@underview.tech>
2022-05-17 22:43:58 -0500

    add systemd recipe to build chain
    Needed for libsystemd,libudev.
    Commit also allows ninja install step to fail
    	- TODO: pinpoint all locations systemd recipe
    	  attempts to install files into
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Build Underview Dependencies
 
-Builds all dependencies needed to develop underview software
+Builds all dependencies used directly when developing underview software
 
 ## Dependencies
 
 ```sh
 $ sudo apt update
 $ sudo apt install -y aptitude
-$ sudo aptitude install -y build-essential cmake clang automake autoconf libtool flex bison m4 yasm
-$ sudo aptitude install -y pkg-config python3-pip ninja-build curl wget git
+$ sudo aptitude install -y build-essential cmake clang automake autoconf libtool flex bison m4 yasm libcap-dev
+$ sudo aptitude install -y pkg-config python3-pip ninja-build curl wget git gperf
 $ sudo python3 -m pip install meson==0.61.4 Mako
 ```
 
---
+++ recipes/systemd.sh
@@ -0,0 +1,70 @@
+# Build systemd v250
+
+
+do_return_version_systemd() {
+  echo "systemd v250"
+}
+
+
+do_return_depends_systemd() {
+  :
+}
+
+
+do_clean_systemd() {
+  rm -rf "${PACKAGES_DIR}/systemd/build"
+}
+
+
+do_fetch_systemd() {
+  msg="Cloning systemd"
+  clone_and_checkout "${PACKAGES_DIR}/systemd" "v250" "https://github.com/systemd/systemd.git" "a420d71793bcbc1539a63be60f83cdc14373ea4a" "${msg}" || return $FAILURE
+  [[ $? -ne 0 ]] && return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_patch_systemd() {
+  :
+}
+
+
+do_configure_systemd() {
+  meson setup \
+        --prefix="$INSTALLPREFIX" \
+        --libdir="${INSTALLPREFIX}/lib" \
+        -Drootprefix="$INSTALLPREFIX" \
+        -Dbashcompletiondir="${INSTALLPREFIX}/usr/share/bash-completion/completions" \
+        -Dzshcompletiondir="${INSTALLPREFIX}/share/zsh/site-functions" \
+        -Dinstall-sysconfdir="false" -Dpolkit="false" \
+        "${PACKAGES_DIR}/systemd/build" \
+        "${PACKAGES_DIR}/systemd" || return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_compile_systemd() {
+  ninja -j $BUILDTHREADS -C "${PACKAGES_DIR}/systemd/build" || return $FAILURE
+  return $SUCCESS
+}
+
+
+# Done on purpose. Allow install step to fail
+# TODO: pinpoint all locations systemd recipe attempts to install files into
+do_install_systemd() {
+  ninja install -C "${PACKAGES_DIR}/systemd/build"
+  return $SUCCESS
+}
+
+
+do_update_artifacts_systemd() {
+  :
+}
+
+
+do_check_is_built_systemd() {
+  [[ -f "${INSTALLPREFIX}/lib/pkgconfig/libsystemd.pc" ]] && return $SUCCESS
+  return $FAILURE
+}
