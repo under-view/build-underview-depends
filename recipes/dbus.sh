861d82f60e482c174ed10e3deeea59c9a9c4cdf1
Vincent Davis Jr <vince@underview.tech>
2022-05-28 21:05:46 -0500

    build: add dbus to build chain
    needed-by:
    	recipe: at-spi2-atk
    		needed-by: gtk
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

---
+++ recipes/dbus.sh
@@ -0,0 +1,65 @@
+# Build dbus v1.13.22
+
+
+do_return_version_dbus() {
+  echo "dbus v1.13.22"
+}
+
+
+do_return_depends_dbus() {
+  :
+}
+
+
+do_clean_dbus() {
+  rm -rf "${PACKAGES_DIR}/dbus/build"
+}
+
+
+do_fetch_dbus() {
+  msg="Cloning dbus"
+  clone_and_checkout "${PACKAGES_DIR}/dbus" "dbus-1.13.22" "https://github.com/freedesktop/dbus.git" "0381298e4565b2dff34c682c8e3d38731d07bc16" "${msg}" || return $FAILURE
+  [[ $? -ne 0 ]] && return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_patch_dbus() {
+  :
+}
+
+
+do_configure_dbus() {
+  cmake -G "${CMAKGENTYPE}" \
+        -S "${PACKAGES_DIR}/dbus" \
+        -B "${PACKAGES_DIR}/dbus/build" \
+        -DDBUS_INSTALL_DIR="${INSTALLPREFIX}" \
+        -DCMAKE_PREFIX_PATH="${INSTALLPREFIX}" \
+        -DCMAKE_INSTALL_PREFIX="${INSTALLPREFIX}" || return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_compile_dbus() {
+  cmake --build "${PACKAGES_DIR}/dbus/build" -j $BUILDTHREADS || return $FAILURE
+  return $SUCCESS
+}
+
+
+do_install_dbus() {
+  cmake --build "${PACKAGES_DIR}/dbus/build" --target install || return $FAILURE
+  return $SUCCESS
+}
+
+
+do_update_artifacts_dbus() {
+  :
+}
+
+
+do_check_is_built_dbus() {
+  [[ -f "${INSTALLPREFIX}/lib/pkgconfig/dbus-1.pc" ]] && return $SUCCESS
+  return $FAILURE
+}
