2436d8e965a3468c19d88074941f85214bc3d1b4
Vincent Davis Jr <vince@underview.tech>
2022-05-28 20:04:14 -0500

    build: add libatk to build chain
    needed-by:
    	recipe: gtk
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

---
+++ recipes/atk.sh
@@ -0,0 +1,64 @@
+# Build atk v2.38.0
+
+
+do_return_version_atk() {
+  echo "atk v2.38.0"
+}
+
+
+do_return_depends_atk() {
+  echo "libffi glib gobject-introspection"
+}
+
+
+do_clean_atk() {
+  rm -rf "${PACKAGES_DIR}/atk/build"
+}
+
+
+do_fetch_atk() {
+  msg="Cloning atk"
+  clone_and_checkout "${PACKAGES_DIR}/atk" "2.38.0" "https://gitlab.gnome.org/GNOME/atk.git" "f1051ba57a2110c46a136a48f4d60a405bc2c3f5" "${msg}" || return $FAILURE
+  [[ $? -ne 0 ]] && return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_patch_atk() {
+  :
+}
+
+
+do_configure_atk() {
+  meson setup \
+        --prefix="${INSTALLPREFIX}" \
+        --libdir="${INSTALLPREFIX}/lib" \
+        "${PACKAGES_DIR}/atk/build" \
+        "${PACKAGES_DIR}/atk" || return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_compile_atk() {
+  ninja -j $BUILDTHREADS -C "${PACKAGES_DIR}/atk/build" || return $FAILURE
+  return $SUCCESS
+}
+
+
+do_install_atk() {
+  ninja install -C "${PACKAGES_DIR}/atk/build" || return $FAILURE
+  return $SUCCESS
+}
+
+
+do_update_artifacts_atk() {
+  :
+}
+
+
+do_check_is_built_atk() {
+  [[ -f "${INSTALLPREFIX}/lib/pkgconfig/atk.pc" ]] && return $SUCCESS
+  return $FAILURE
+}
