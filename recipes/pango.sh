223fb390e518f852be876fa122a04051a4a58b6d
Vincent Davis Jr <vince@underview.tech>
2022-05-28 19:12:31 -0500

    build: add pango to build chain
    needed-by:
    	recipe: gtk
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

---
+++ recipes/pango.sh
@@ -0,0 +1,68 @@
+# Build pango v1.50.7
+
+
+do_return_version_pango() {
+  echo "pango v1.50.7"
+}
+
+
+do_return_depends_pango() {
+  echo "libffi glib gobject-introspection freetype harfbuzz fontconfig cairo fribidi"
+}
+
+
+do_clean_pango() {
+  rm -rf "${PACKAGES_DIR}/pango/build"
+}
+
+
+do_fetch_pango() {
+  msg="Cloning pango"
+  clone_and_checkout "${PACKAGES_DIR}/pango" "1.50.7" "https://gitlab.gnome.org/GNOME/pango.git" "a2ccd36a42e039d3600b04fe37fdc47f267d90c7" "${msg}" || return $FAILURE
+  [[ $? -ne 0 ]] && return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_patch_pango() {
+  :
+}
+
+
+do_configure_pango() {
+  rm -rf "${PACKAGES_DIR}/pango/subprojects"
+
+  meson setup \
+        --prefix="${INSTALLPREFIX}" \
+        --libdir="${INSTALLPREFIX}/lib" \
+        -Dcairo="enabled" \
+        -Dsysprof="disabled" -Dlibthai="disabled" \
+        "${PACKAGES_DIR}/pango/build" \
+        "${PACKAGES_DIR}/pango" || return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_compile_pango() {
+  ninja -j $BUILDTHREADS -C "${PACKAGES_DIR}/pango/build" || return $FAILURE
+  return $SUCCESS
+}
+
+
+do_install_pango() {
+  ninja install -C "${PACKAGES_DIR}/pango/build" || return $FAILURE
+  return $SUCCESS
+}
+
+
+do_update_artifacts_pango() {
+  :
+}
+
+
+do_check_is_built_pango() {
+  [[ -f "${INSTALLPREFIX}/lib/pkgconfig/pango.pc" ]] && return $SUCCESS
+  return $FAILURE
+}
