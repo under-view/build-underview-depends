e070a4ff21c4b9678f39af8e5b0e29c8bf8069d7
Vincent Davis Jr <vince@underview.tech>
2022-06-07 23:57:08 -0500

    build: add libgudev to build chain
    A subdepency of libwacom. libwacom
    needed-by:
    	recipe: libinput
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

---
+++ recipes/libgudev.sh
@@ -0,0 +1,66 @@
+# Build libgudev v1.0
+
+
+do_return_version_libgudev() {
+  echo "libgudev v1.0"
+}
+
+
+do_return_depends_libgudev() {
+  :
+}
+
+
+do_clean_libgudev() {
+  rm -rf "${PACKAGES_DIR}/libgudev/build"
+}
+
+
+do_fetch_libgudev() {
+  msg="Cloning libgudev"
+  clone_and_checkout "${PACKAGES_DIR}/libgudev" "237" "https://gitlab.gnome.org/GNOME/libgudev.git" "dff7a794e55d6ad7a10e8edacd73aa047593e74c" "${msg}" || return $FAILURE
+  [[ $? -ne 0 ]] && return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_patch_libgudev() {
+  :
+}
+
+
+do_configure_libgudev() {
+  meson setup \
+        --prefix="${INSTALLPREFIX}" \
+        --libdir="${INSTALLPREFIX}/lib" \
+        -Dtests="disabled" \
+        -Dvapi="disabled" \
+        "${PACKAGES_DIR}/libgudev/build" \
+        "${PACKAGES_DIR}/libgudev" || return $FAILURE
+
+  return $SUCCESS
+}
+
+
+do_compile_libgudev() {
+  ninja -j $BUILDTHREADS -C "${PACKAGES_DIR}/libgudev/build" || return $FAILURE
+  return $SUCCESS
+}
+
+
+do_install_libgudev() {
+  ninja install -C "${PACKAGES_DIR}/libgudev/build" || return $FAILURE
+  return $SUCCESS
+}
+
+
+do_update_artifacts_libgudev() {
+  :
+}
+
+
+do_check_is_built_libgudev() {
+  [[ -f "${INSTALLPREFIX}/lib/libgudev-1.0.so" ]] && return $SUCCESS
+  return $FAILURE
+}
