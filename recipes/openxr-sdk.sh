99d83bbb3eb82884f9dbf03f4d140b194b36183b
Vincent Davis Jr <vince@underview.tech>
2022-10-07 20:13:20 -0500

    openxr-sdk: update folder name
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- recipes/openxr-sdk.sh
+++ recipes/openxr-sdk.sh
@@ -1,8 +1,8 @@
-# Build OpenXR-SDK v1.0.25
+# Build openxr-sdk v1.0.25
 
 
 do_return_version_openxr-sdk() {
-  echo "OpenXR-SDK v1.0.25"
+  echo "openxr-sdk v1.0.25"
 }
 
 
@@ -12,13 +12,13 @@ do_return_depends_openxr-sdk() {
 
 
 do_clean_openxr-sdk() {
-  rm -rf "${PACKAGES_DIR}/OpenXR-SDK/build"
+  rm -rf "${PACKAGES_DIR}/openxr-sdk/build"
 }
 
 
 do_fetch_openxr-sdk() {
-  msg="Cloning OpenXR-SDK"
-  clone_and_checkout "${PACKAGES_DIR}/OpenXR-SDK" "release-1.0.25" "https://github.com/KhronosGroup/OpenXR-SDK.git" "c16a18c99740ea5dd251e3af117e0e5aea4ceaa9" "${msg}" || return $FAILURE
+  msg="Cloning openxr-sdk"
+  clone_and_checkout "${PACKAGES_DIR}/openxr-sdk" "release-1.0.25" "https://github.com/KhronosGroup/OpenXR-SDK.git" "c16a18c99740ea5dd251e3af117e0e5aea4ceaa9" "${msg}" || return $FAILURE
   [[ $? -ne 0 ]] && return $FAILURE
 
   return $SUCCESS
@@ -32,8 +32,8 @@ do_patch_openxr-sdk() {
 
 do_configure_openxr-sdk() {
   cmake -G "${CMAKGENTYPE}" \
-        -S "${PACKAGES_DIR}/OpenXR-SDK" \
-        -B "${PACKAGES_DIR}/OpenXR-SDK/build" \
+        -S "${PACKAGES_DIR}/openxr-sdk" \
+        -B "${PACKAGES_DIR}/openxr-sdk/build" \
         -DCMAKE_BUILD_TYPE=Release \
         -DBUILD_SHARED_LIBS=ON \
         -DCMAKE_PREFIX_PATH="${INSTALLPREFIX}" \
@@ -44,13 +44,13 @@ do_configure_openxr-sdk() {
 
 
 do_compile_openxr-sdk() {
-  cmake --build "${PACKAGES_DIR}/OpenXR-SDK/build" -j $BUILDTHREADS || return $FAILURE
+  cmake --build "${PACKAGES_DIR}/openxr-sdk/build" -j $BUILDTHREADS || return $FAILURE
   return $SUCCESS
 }
 
 
 do_install_openxr-sdk() {
-  cmake --build "${PACKAGES_DIR}/OpenXR-SDK/build" --target install || return $FAILURE
+  cmake --build "${PACKAGES_DIR}/openxr-sdk/build" --target install || return $FAILURE
   return $SUCCESS
 }
 
