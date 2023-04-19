c2219bbbe3764eea2139d40cd4db1e007c91f319
Vincent Davis Jr <vince@underview.tech>
2023-04-19 00:02:59 -0500

    hwdata: upgrade v0.348-9.8 -> v0.369
    Also removes the make download command.
    Odd reasons patches applied on ids failing.
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- recipes/hwdata.sh
+++ recipes/hwdata.sh
@@ -1,8 +1,8 @@
-# Build hwdata v0.348-9.8
+# Build hwdata v0.369
 
 
 do_return_version_hwdata() {
-	echo "hwdata v0.348-9.8"
+	echo "hwdata v0.369"
 }
 
 
@@ -18,7 +18,7 @@ do_clean_hwdata() {
 
 do_fetch_hwdata() {
 	msg="Cloning hwdata"
-	clone_and_checkout "${PACKAGES_DIR}/hwdata" "v0.348-9.8" "https://github.com/vcrhonek/hwdata.git" "e098f2a3f626c46835206afca262d0a7a1226a94" "${msg}" || return $FAILURE
+	clone_and_checkout "${PACKAGES_DIR}/hwdata" "v0.369" "https://github.com/vcrhonek/hwdata.git" "ced93f9d1c15e45e35378e82b8da96027e58174a" "${msg}" || return $FAILURE
 	[[ $? -ne 0 ]] && return $FAILURE
 
 	return $SUCCESS
@@ -46,7 +46,6 @@ do_configure_hwdata() {
 
 
 do_compile_hwdata() {
-	make -C "${PACKAGES_DIR}/hwdata" download || return $FAILURE
 	make -C "${PACKAGES_DIR}/hwdata" -j $BUILDTHREADS || return $FAILURE
 	return $SUCCESS
 }
