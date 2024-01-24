e3f53a9441d99c1546d5d52979883b088bd67b61
Vincent Davis Jr <vince@underview.tech>
2024-01-24 00:15:37 -0500

    shaderc: upgreade v2022.1 -> v2023.7
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- recipes/shaderc.sh
+++ recipes/shaderc.sh
@@ -1,13 +1,14 @@
-# Build shaderc v2022.1
+# Build shaderc v2023.7
 
+PV="v2023.7"
 
 do_return_version_shaderc() {
-	echo "shaderc v2022.1"
+	echo "shaderc ${PV}"
 }
 
 
 do_return_depends_shaderc() {
-	echo "spirv-headers spirv-tools gslang"
+	echo "spirv-headers spirv-tools glslang"
 }
 
 
@@ -19,7 +20,7 @@ do_clean_shaderc() {
 
 do_fetch_shaderc() {
 	msg="Cloning shaderc"
-	clone_and_checkout "${PACKAGES_DIR}/shaderc" "v2022.1" "https://github.com/google/shaderc.git" "1bbf43f210941ba69a2cd05cf3529063f1ff5bb9" "${msg}" || return $FAILURE
+	clone_and_checkout "${PACKAGES_DIR}/shaderc" "${PV}" "https://github.com/google/shaderc.git" "3882b16417077aa8eaa7b5775920e7ba4b8a224d" "${msg}"
 	[[ $? -ne 0 ]] && return $FAILURE
 
 	return $SUCCESS
@@ -27,7 +28,10 @@ do_fetch_shaderc() {
 
 
 do_patch_shaderc() {
-	git -C "${PACKAGES_DIR}/shaderc" apply "${PATCHES_DIR}/shaderc/0001-CMakeLists-comment-add_subdirectory-third_party-line.patch" > /dev/null 2>&1
+	git -C "${PACKAGES_DIR}/shaderc" apply "${PATCHES_DIR}/shaderc/0001-CMakeLists.txt-drop-OSDependent-OGLCompiler-from-lis.patch" > /dev/null 2>&1
+	[[ $? -ne 0 ]] && return $FAILURE
+
+	git -C "${PACKAGES_DIR}/shaderc" apply "${PATCHES_DIR}/shaderc/0001-cmake-disable-building-external-dependencies.patch" > /dev/null 2>&1
 	[[ $? -ne 0 ]] && return $FAILURE
 
 	git -C "${PACKAGES_DIR}/shaderc" apply "${PATCHES_DIR}/shaderc/0002-glslc-libshaderc-util-update-compiler-include-paths.patch" > /dev/null 2>&1
@@ -41,12 +45,15 @@ do_configure_shaderc() {
 	cmake -G "${CMAKEGENTYPE}" \
 	      -S "${PACKAGES_DIR}/shaderc" \
 	      -B "${PACKAGES_DIR}/shaderc/build" \
+	      -DBUILD_EXTERNAL="OFF" \
 	      -DBUILD_SHARED_LIBS="ON" \
 	      -DSHADERC_SKIP_TESTS="ON" \
 	      -DSHADERC_SKIP_EXAMPLES="ON" \
+	      -DSHADERC_SKIP_COPYRIGHT_CHECK="ON" \
 	      -Dglslang_SOURCE_DIR="${PACKAGES_DIR}/glslang" \
 	      -Dshaderc_SOURCE_DIR="${PACKAGES_DIR}/shaderc" \
 	      -Dspirv-tools_SOURCE_DIR="${PACKAGES_DIR}/spirv-tools" \
+	      -DCMAKE_CXX_STANDARD="17" \
 	      -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
 	      -DCMAKE_PREFIX_PATH="${INSTALLPREFIX}" \
 	      -DCMAKE_INSTALL_LIBDIR="${INSTALLPREFIX}/lib" \
